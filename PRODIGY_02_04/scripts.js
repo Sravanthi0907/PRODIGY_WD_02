68a7fff189f9200c478dd1008591476bffe6f07f
SRAVANTHI <168658029+Sravanthi0907@users.noreply.github.com>
2024-07-29 21:30:18 +0530

    Add files via upload

---
+++ PRODIGY_02_04/scripts.js
@@ -0,0 +1,20 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Prevent default anchor behavior
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
